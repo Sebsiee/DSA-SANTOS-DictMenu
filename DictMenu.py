c3e6e67a20d35f995c1c42f149556943211ec374
unknown <santosbasty4@gmail.com>
2022-11-12 11:53:18 +0800

    Third Commit - Information, Dictionary

--- DictMenu.py
+++ DictMenu.py
@@ -3,9 +3,32 @@ print("Menu:")
 print("1 -> Add an item")
 print("2 -> Search")
 print("3 -> Exit (y/n)")
+dict = {}
 # Ask user to select item
-user_input = int(input("What do you want to do? (1-3) "))
+user_input = input("What do you want to do? (1-3) ")
 # Perform option
 # Option 1 Ask personal data
+if user_input == "1":
+    print("Add your information.")
+    name = "Name"
+    user_name = input("Full Name: ")
+    dict[name] = user_name
+
+    age = "Age"
+    user_age = input("Age: ")
+    dict[age] = user_age
+
+    address = "Address"
+    user_address = input("Address: ")
+    dict[address] = user_address
+    
+    dict = {user_name  : {age : user_age, address : user_address}}
+    print("Saved!")
+
+    print(dict)
+
+else:
+    print("Input not recognized.")
+
 # Option 2 Search
 # Option 3 Ask User
