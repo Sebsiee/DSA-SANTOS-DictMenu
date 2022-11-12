ab4579bb74d39d4005fcdbd7d0363559d67ee91a
unknown <santosbasty4@gmail.com>
2022-11-12 11:08:56 +0800

    Second Commit - User Input

--- DictMenu.py
+++ DictMenu.py
@@ -4,6 +4,7 @@ print("1 -> Add an item")
 print("2 -> Search")
 print("3 -> Exit (y/n)")
 # Ask user to select item
+user_input = int(input("What do you want to do? (1-3) "))
 # Perform option
 # Option 1 Ask personal data
 # Option 2 Search
