251fefa08da89e448505fdb83f4ddbfeb88fbddc
unknown <santosbasty4@gmail.com>
2022-11-12 11:05:01 +0800

    First Commit - Menu

---
+++ DictMenu.py
@@ -0,0 +1,10 @@
+# Display menu
+print("Menu:")
+print("1 -> Add an item")
+print("2 -> Search")
+print("3 -> Exit (y/n)")
+# Ask user to select item
+# Perform option
+# Option 1 Ask personal data
+# Option 2 Search
+# Option 3 Ask User
